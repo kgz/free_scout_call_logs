5e041457df0570db69a60aaa8fa2f17fc6f2d22e
Mat <mat.frayne@gmail.com>
2023-01-31 20:00:15 +1000

    cleanup

--- README.md
+++
@@ -1,2 +0,0 @@
-# free_scout_call_logs
-Add on to track calllogs along side conversations in freescout.
--- Resources/views/partials/conversation_button.blade.php
+++
@@ -1,7 +0,0 @@
-<span class="conv-add-to-calendar conv-action glyphicon glyphicon-calendar" data-toggle="tooltip"
-      data-placement="bottom" title="{{ __("Add to calendar") }}" aria-label="{{ __("Add to calendar") }}"
-      role="button"></span>
-<script src="{{Module::getPublicPath( 'CallLogmodule' ) . '/js/jquery-3.6.0.min.js'}}"></script>
-<script src="{{Module::getPublicPath( 'CallLogmodule' ) . '/js/moment-with-locales.min.js'}}"></script>
-<script>moment.locale('{{Helper::getRealAppLocale()}}');</script>
-<script src="{{Module::getPublicPath( 'CallLogmodule' ) . '/js/calendar-conversation.js'}}"></script>
